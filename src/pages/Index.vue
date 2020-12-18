f6b07ab219d2f88327d8386c91e5aa041cb12df5
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2020-12-19 02:30:57 +0800

    :rocket: minor changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,12 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <router-link to="/">reset</router-link>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex"
+};
 </script>
